a914acc31c75fd303bea343926180065b4e4921b
yurifillippo <yuri.fillippo@outlook.com>
2025-07-01 18:03:34 -0300

    teste de execucao

--- function_app.py
+++ function_app.py
@@ -3,43 +3,34 @@ import azure.functions as func
 from datetime import datetime
 import os
 import requests
-#import pandas as pd
+import pandas as pd
 import io
-#from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient
 
-# Configuração global do logger
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-
+# Configuração do logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-
 app = func.FunctionApp()
-
 
 @app.timer_trigger(schedule="0 5 * * * *", arg_name="myTimer", run_on_startup=False,
                    use_monitor=False)
 def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
-    logging.info('Python timer trigger function started.')
-
-    logging.info('Configuração do logger')
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
-
-    logger = logging.getLogger(__name__)
 
     logging.info('Variaveis que se alteram por conjunto de dados')
     url = "https://inradar.com.br/api/v1/member_history"
-    params = {"limit": 50, "offset": 0}
+
+    params = {
+        "limit": 50,
+        "offset": 0
+    }
+
     name_file = "member_history"
+
     data_formatada = datetime.today().strftime('%Y%m%d')
 
-    ACCOUNT_NAME = os.environ.get('ACCOUNT_NAME')
-    ACCOUNT_KEY = os.environ.get('ACCOUNT_KEY')
+    ACCOUNT_NAME = "adairawdatastore"
+    ACCOUNT_KEY = "9rpvbn2UDWIdEZ7CxbuVZwNCjU0PSJ2X1BYqukn2fSULlmRfs3r/nxUYBSqMOocEKoxGlUl+5KxM+AStni+Uvw=="
     CONTAINER_NAME = "raw"
     BLOB_NAME = f"{name_file}/{name_file}_{data_formatada}.csv"
 
@@ -96,10 +87,10 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         logging.info('Converte todos os dados em DataFrame')
         df = pd.json_normalize(todos_size)
 
-        print(f"Quantidade de dados: {df.shape[0]}")
+        logging.info(f"Quantidade de dados: {df.shape[0]}")
 
         logger.info("Colunas disponíveis:")
-        print(df.columns.tolist())
+        logging.info(df.columns.tolist())
 
         logging.info('Salvar tudo')
         buffer = io.BytesIO()
@@ -107,12 +98,13 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         buffer.seek(0)
 
         logging.info('Conectar ao Azure Blob Storage')
+
         # Conectar ao Azure Blob Storage
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
 
         # Enviar o arquivo para o blob
-        blob_client.upload_blob(buffer, blob_type="BlockBlob", overwrite=True, max_concurrency=4)
+        blob_client.upload_blob(buffer, overwrite=True)
 
         logger.info("Arquivo completo salvo como '{name_file}.csv'")
     else:
