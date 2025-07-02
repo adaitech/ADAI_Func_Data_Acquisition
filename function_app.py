f9ed707bf31ccc0ffab39c970d057e9178607856
Jonatan Cardoso Saraiva <jonatan.saraiva@hdi-yelum.com.br>
2025-07-01 21:05:07 -0300

    teste código anterior

--- function_app.py
+++ function_app.py
@@ -45,10 +45,14 @@ def Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         "offset": 0
     }
 
+    # Arquivo de saída que será salvo no Azure Blob Storage
+    logger.info('Arquivo de saída que será salvo no Azure Blob Storage')
     name_file = "member_history"
 
     data_formatada = datetime.today().strftime('%Y%m%d')
 
+    # Dados de acesso ao Azure Blob Storage
+    logger.info('Dados de acesso ao Azure Blob Storage')
     ACCOUNT_NAME = "adairawdatastore"
     ACCOUNT_KEY = "9rpvbn2UDWIdEZ7CxbuVZwNCjU0PSJ2X1BYqukn2fSULlmRfs3r/nxUYBSqMOocEKoxGlUl+5KxM+AStni+Uvw=="
     CONTAINER_NAME = "raw"
@@ -58,6 +62,8 @@ def Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
     logger.info(f'ACCOUNT_KEY: {ACCOUNT_KEY}')
     logger.info(f'BLOB_NAME: {BLOB_NAME}')
 
+    # Construir a connection string
+    # A connection string é usada para autenticar e acessar o Azure Blob Storage
     logger.info('Construir a connection string')
     connection_string = (
         f"DefaultEndpointsProtocol=https;"
@@ -66,7 +72,8 @@ def Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         f"EndpointSuffix=core.windows.net"
     )
 
-    logger.info('Cabeçalhos de autenticação')
+    # Cabecalhos de autenticação para o sistema da InChurch
+    logger.info('Cabeçalhos de autenticação para o sistema da InChurch')
     headers = {
         "Authorization": "ApiKey yurifillippo:9d20cd67a03a19a2bbcf7cac0ea11b2409e11bc7",
         "Content-Type": "application/json;charset=UTF-8",
@@ -100,11 +107,36 @@ def Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         todos_size.extend(data)
         params["offset"] += params["limit"]
 
-    logger.info('Se dados foram coletados')
+    logging.info('Se dados foram coletados')
+    if todos_size:
 
-    logger.info('Se dados foram coletados')
+        logging.info('Converte todos os dados em DataFrame')
+        df = pd.json_normalize(todos_size)
 
-    logger.info('Python timer trigger function executed.')
+        print(f"Quantidade de dados: {df.shape[0]}")
+
+        logger.info("Colunas disponíveis:")
+        print(df.columns.tolist())
+
+        logging.info('Salvar tudo')
+        buffer = io.BytesIO()
+        df.to_csv(buffer, index=False)
+        buffer.seek(0)
+
+        logging.info('Conectar ao Azure Blob Storage')
+        blob_service_client = BlobServiceClient.from_connection_string(
+            connection_string)
+        blob_client = blob_service_client.get_blob_client(
+            container=CONTAINER_NAME, blob=BLOB_NAME)
+
+        logger.info('Enviar o arquivo para o blob')
+        blob_client.upload_blob(buffer, overwrite=True)
+
+        logger.info("Arquivo completo salvo como '{name_file}.csv'")
+    else:
+        logger.info("Nenhum dado coletado da API.")
+
+    logging.info('Python timer trigger function executed.')
 
 
 # if __name__ == '__main__':
