cfec52763934aff7c3c96c1d7418a7d0d218520d
Jonatan Cardoso Saraiva <jonatan.saraiva@hdi-yelum.com.br>
2025-07-01 21:29:18 -0300

    código yuri

--- function_app.py
+++ function_app.py
@@ -1,9 +1,9 @@
 import logging
 import azure.functions as func
-# from datetime import datetime
-# import os
+from datetime import datetime
+import os
+import io
 # import requests
-# import io
 # import pandas as pd
 # from azure.storage.blob import BlobServiceClient
 
@@ -36,28 +36,28 @@ def flatten_dict(d, parent_key='', sep='.'):
 def Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function started.')
 
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    # Configuração do logger
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
-    logger = logging.getLogger(__name__)
-
-    # Variaveis que se alteram por conjunto de dados
+    logging.info('Variaveis que se alteram por conjunto de dados')
     url = "https://inradar.com.br/api/v1/member_history"
-    params = {"limit": 50, "offset": 0}
+
+    params = {
+        "limit": 50,
+        "offset": 0
+    }
+
     name_file = "member_history"
+
     data_formatada = datetime.today().strftime('%Y%m%d')
 
-    ACCOUNT_NAME = os.environ.get(ACCOUNT_NAME)
-    ACCOUNT_KEY = os.environ.get(ACCOUNT_KEY)
+    ACCOUNT_NAME = "adairawdatastore"
+    ACCOUNT_KEY = "9rpvbn2UDWIdEZ7CxbuVZwNCjU0PSJ2X1BYqukn2fSULlmRfs3r/nxUYBSqMOocEKoxGlUl+5KxM+AStni+Uvw=="
     CONTAINER_NAME = "raw"
     BLOB_NAME = f"{name_file}/{name_file}_{data_formatada}.csv"
 
-    # Construir a connection string
+    logging.info(f'ACCOUNT_NAME: {ACCOUNT_NAME}')
+    logging.info(f'ACCOUNT_KEY: {ACCOUNT_KEY}')
+    logging.info(f'BLOB_NAME: {BLOB_NAME}')
+
+    logging.info('Construir a connection string')
     connection_string = (
         f"DefaultEndpointsProtocol=https;"
         f"AccountName={ACCOUNT_NAME};"
@@ -65,65 +65,85 @@ def Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         f"EndpointSuffix=core.windows.net"
     )
 
-    # Cabeçalhos de autenticação
+    logging.info('Cabeçalhos de autenticação')
     headers = {
         "Authorization": "ApiKey yurifillippo:9d20cd67a03a19a2bbcf7cac0ea11b2409e11bc7",
         "Content-Type": "application/json;charset=UTF-8",
         "Channel": "control_panel",
         "Origin": "https://admin.inchurch.com.br",
         "Referer": "https://admin.inchurch.com.br/",
-        "Accept": "application/json, text/plain, /"
+        "Accept": "application/json, text/plain, */*"
     }
 
     todos_size = []
 
     while True:
-        logger.info(f"Linhas de dados coletadas: {params['offset']}")
-        response = requests.get(url, headers=headers, params=params)
+        logging.info(f"Linhas de dados coletadas: {params['offset']}")
+        response = requests.get(url, headers=headers,
+                                params=params, verify=False)
 
         try:
             data = response.json()
         except Exception as e:
-            logger.error("Erro ao converter JSON:", e)
-            logger.error("Resposta crua:", response.text)
+            logging.error("Erro ao converter JSON:", e)
+            logging.error("Resposta crua:", response.text)
             break
+
+        logging.info('data' + str(data))
 
         data = data.get("objects", [])  # Ponto-chave da resposta
 
         if not data:
-            logger.info("Nenhum dado retornado. Fim da coleta.")
+            logging.info("Nenhum dado retornado. Fim da coleta.")
             break
 
         todos_size.extend(data)
         params["offset"] += params["limit"]
 
-    # Se dados foram coletados
+    logging.info('Se dados foram coletados')
     if todos_size:
-        # Converte todos os dados em DataFrame
-        df = pd.json_normalize(todos_size)
 
-        print(f"Quantidade de dados: {df.shape[0]}")
+        logging.info('Flatten dos dados')
+        flattened_data = [flatten_dict(item) for item in todos_size]
 
-        logger.info("Colunas disponíveis:")
-        print(df.columns.tolist())
+        logging.info(
+            f"Quantidade de registros coletados: {len(flattened_data)}")
 
-        # Salvar tudo
-        buffer = io.BytesIO()
-        df.to_csv(buffer, index=False)
-        buffer.seek(0)
+        if flattened_data:
+            # Pegar todos os campos (headers) presentes nos dados flatten
+            all_keys = set()
+            for item in flattened_data:
+                all_keys.update(item.keys())
 
-        # Conectar ao Azure Blob Storage
-        blob_service_client = BlobServiceClient.from_connection_string(
-            connection_string)
-        blob_client = blob_service_client.get_blob_client(
-            container=CONTAINER_NAME, blob=BLOB_NAME)
+            # Organiza as colunas em ordem alfabética (opcional)
+            fieldnames = sorted(list(all_keys))
 
-        # Enviar o arquivo para o blob
-        blob_client.upload_blob(buffer, overwrite=True)
+            logging.info("Colunas disponíveis: %s", fieldnames)
 
-        logger.info(f"Arquivo completo salvo como '{name_file}.csv'")
+            # Escreve CSV no buffer
+            buffer = io.StringIO()
+            writer = csv.DictWriter(buffer, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(flattened_data)
+
+            csv_data = buffer.getvalue().encode('utf-8')
+            bytes_buffer = io.BytesIO(csv_data)
+
+            # Conectar ao Azure Blob Storage
+            logging.info('Conectar ao Azure Blob Storage')
+            blob_service_client = BlobServiceClient.from_connection_string(
+                connection_string)
+            blob_client = blob_service_client.get_blob_client(
+                container=CONTAINER_NAME, blob=BLOB_NAME)
+
+            # Enviar o arquivo para o blob
+            blob_client.upload_blob(bytes_buffer, overwrite=True)
+
+            logging.info("Arquivo completo salvo como '%s'", BLOB_NAME)
+        else:
+            logging.info("Não há dados para salvar.")
     else:
-        logger.info("Nenhum dado coletado da API.")
+        logging.info("Nenhum dado coletado da API.")
 
     logging.info('Python timer trigger function executed.')
 
