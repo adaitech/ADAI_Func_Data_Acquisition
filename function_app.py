854808b60d43eba74e7b62f5a0577fc9190d96d9
Jonatan Cardoso Saraiva <jonatan.saraiva@hdi-yelum.com.br>
2025-07-01 21:16:45 -0300

    teste

--- function_app.py
+++ function_app.py
@@ -1,12 +1,5 @@
 import logging
 import azure.functions as func
-from datetime import datetime
-import os
-import requests
-# import pandas as pd
-import io
-# from azure.storage.blob import BlobServiceClient
-
 
 app = func.FunctionApp()
 
@@ -19,30 +12,25 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Configuração do logger')
+    # Configuração do logger
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
-
     logger = logging.getLogger(__name__)
 
-    logging.info('Variaveis que se alteram por conjunto de dados')
+    # Variaveis que se alteram por conjunto de dados
     url = "https://inradar.com.br/api/v1/member_history"
     params = {"limit": 50, "offset": 0}
     name_file = "member_history"
     data_formatada = datetime.today().strftime('%Y%m%d')
 
-    ACCOUNT_NAME = os.environ.get('ACCOUNT_NAME')
-    ACCOUNT_KEY = os.environ.get('ACCOUNT_KEY')
+    ACCOUNT_NAME = os.environ.get(ACCOUNT_NAME)
+    ACCOUNT_KEY = os.environ.get(ACCOUNT_KEY)
     CONTAINER_NAME = "raw"
     BLOB_NAME = f"{name_file}/{name_file}_{data_formatada}.csv"
 
-    logging.info('ACCOUNT_NAME' + ACCOUNT_NAME)
-    logging.info('ACCOUNT_KEY' + ACCOUNT_KEY)
-    logging.info('BLOB_NAME' + BLOB_NAME)
-
-    logging.info('Construir a connection string')
+    # Construir a connection string
     connection_string = (
         f"DefaultEndpointsProtocol=https;"
         f"AccountName={ACCOUNT_NAME};"
@@ -50,7 +38,7 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         f"EndpointSuffix=core.windows.net"
     )
 
-    logging.info('Cabeçalhos de autenticação')
+    # Cabeçalhos de autenticação
     headers = {
         "Authorization": "ApiKey yurifillippo:9d20cd67a03a19a2bbcf7cac0ea11b2409e11bc7",
         "Content-Type": "application/json;charset=UTF-8",
@@ -73,8 +61,6 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
             logger.error("Resposta crua:", response.text)
             break
 
-        logging.info('data' + str(data))
-
         data = data.get("objects", [])  # Ponto-chave da resposta
 
         if not data:
@@ -84,10 +70,9 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         todos_size.extend(data)
         params["offset"] += params["limit"]
 
-    logging.info('Se dados foram coletados')
+    # Se dados foram coletados
     if todos_size:
-
-        logging.info('Converte todos os dados em DataFrame')
+        # Converte todos os dados em DataFrame
         df = pd.json_normalize(todos_size)
 
         print(f"Quantidade de dados: {df.shape[0]}")
@@ -95,21 +80,21 @@ def ADAI_Func_Data_Acquisition(myTimer: func.TimerRequest) -> None:
         logger.info("Colunas disponíveis:")
         print(df.columns.tolist())
 
-        logging.info('Salvar tudo')
+        # Salvar tudo
         buffer = io.BytesIO()
         df.to_csv(buffer, index=False)
         buffer.seek(0)
 
-        logging.info('Conectar ao Azure Blob Storage')
+        # Conectar ao Azure Blob Storage
         blob_service_client = BlobServiceClient.from_connection_string(
             connection_string)
         blob_client = blob_service_client.get_blob_client(
             container=CONTAINER_NAME, blob=BLOB_NAME)
 
-        logger.info('Enviar o arquivo para o blob')
+        # Enviar o arquivo para o blob
         blob_client.upload_blob(buffer, overwrite=True)
 
-        logger.info("Arquivo completo salvo como '{name_file}.csv'")
+        logger.info(f"Arquivo completo salvo como '{name_file}.csv'")
     else:
         logger.info("Nenhum dado coletado da API.")
 
